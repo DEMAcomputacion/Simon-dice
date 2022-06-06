1707d6d3a61b1838cbc7e2ddefdb923df39af0aa
DEMAcomputacion <105241040+DEMAcomputacion@users.noreply.github.com>
2022-06-06 19:47:36 -0300

    se cambia la forma en la que se reproduce la secuencia de la maquina
    la forma en la que se media el tiempo entre sonido y sonido de la secuencia se cambio por otra funcion (setInterval) para que quede mas claro.

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const sonido2 = new Audio();
 const sonido3 = new Audio();
 const sonido4 = new Audio();
 const error = new Audio();
+let secuencia = new Object;
 let nroRonda;
 let nroClick = 1;
+let intervaloId;
+let idSecuencia = 1;
 
 sonido1.src = "verde.mp3";
 sonido2.src = "rojo.mp3";
@@ -12,14 +15,16 @@ sonido3.src = "azul.mp3";
 sonido4.src = "amarillo.mp3";
 error.src = "error.mp3"
 
+const $tableroRonda = document.querySelector("#nroRonda");
 const $turnos = document.querySelector("#turnos");
 const $boton1 = document.querySelector("#btnVerde");
 const $boton2 = document.querySelector("#btnRojo");
 const $boton3 = document.querySelector("#btnAzul");
 const $boton4 = document.querySelector("#btnAmarillo");
 const $botonStart = document.querySelector("#btnStart");
+const $tituloRonda = document.querySelector("#tituloRonda");
 
-let secuencia = new Object;
+
 const botones = {
     1: $boton1,
     2: $boton2,
@@ -51,11 +56,12 @@ $botonStart.onclick = function(){
     desablitarStart();
     nroRonda = 1;
     document.querySelector("#body").className = "gradiente";
-    document.querySelector("#tituloRonda").className = "";
-    document.querySelector("#tituloRonda").innerText = "Ronda:";
-    document.querySelector("#nroRonda").className = "score";
-    document.querySelector("#nroRonda").innerText = nroRonda;
+    $tituloRonda.className = "";
+    $tituloRonda.innerText = "Ronda:";
+    $tableroRonda.className = "score";
+    $tableroRonda.innerText = nroRonda;
     $turnos.innerText = "Memoriza Esto!!!";
+    Object.keys(botones).forEach(function(key){botones[key].disabled = false});
     window.setTimeout(rondaPc, 1000);
 }
 
@@ -70,42 +76,32 @@ function habilitarStart(){
 function rondaPc(){
         let random = generaRandom();
         secuencia[nroRonda] = random;
-
         reproduceSecuencia();
-
-        document.querySelector("#nroRonda").innerText = nroRonda;
+        $tableroRonda.innerText = nroRonda;
         window.setTimeout(rondaUsuario, 1000, secuencia);
 }
 
 function reproduceSecuencia(){
-    
-    Object.keys(secuencia).forEach(function(i){
-        const key = secuencia[i];
-        const botonActual = botones[key];
+        intervaloId = window.setInterval(leeSecuencia, 700)
+}
+
+function leeSecuencia(){
+    if(idSecuencia <= Object.keys(secuencia).length){
+        const key = secuencia[idSecuencia];
         const reproducir = sonidos[key];
-        window.setTimeout(brillaBoton, 0, key, botonActual);
-        window.setTimeout(reproduceSonido, 0, reproducir);
-        window.setTimeout(retornaColor, 0, key, botonActual);
-    });
-    
+        reproducir.play();
+        const botonActual = botones[key];
+        botonActual.className = `boton${key}Brilla`;
+        window.setTimeout(borraBrillo, 200,key, botonActual)
+        idSecuencia ++;
+    }else{
+    clearInterval(intervaloId);
+    idSecuencia = 1;
+    }
 }
 
-function reproduceSonido(reproducir) {
-    reproducir.play();
-    time(0.3);
-    return;
-}
-
-function brillaBoton(key,botonActual){
-    botonActual.className = `boton${key}Brilla`;
-    time(0.3);
-    return;
-}
-
-function retornaColor(key, botonActual){
+function borraBrillo(key, botonActual){
     botonActual.className = `boton${key}`;
-    time(0.3);
-    return;
 }
 
 function rondaUsuario(secuencia){
@@ -154,8 +150,9 @@ function chequeaGame(botonActual){
 function gameOver(){
     secuencia = {};
     document.querySelector("#body").className = "gradienteGameOver";
-    document.querySelector("#nroRonda").className = "oculto";
-    document.querySelector("#tituloRonda").innerText = `Alcanzaste\n${nroRonda}\nrondas`;
+    $tableroRonda.className = "oculto";
+    $tituloRonda.innerText = `Alcanzaste\n${nroRonda}\nrondas`;
     habilitarStart();
+    Object.keys(botones).forEach(function(key){botones[key].disabled = true});
     nroRonda = 1;
 }
